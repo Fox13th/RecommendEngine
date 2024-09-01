8cc6bff57797bb47259223d9b17f0498c835f216
Andrew <narnianlion@yandex.ru>
2024-09-01 20:23:46 +0300

    1. Запись результатов в Redis; 2. Работа с .env

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import json
 import logging
+import os
 from abc import ABC, abstractmethod
 
 import pandas as pd
+import redis
+from dotenv import load_dotenv
 
 from surprise import SVD, accuracy
 from surprise import Dataset, Reader
 from surprise.model_selection import train_test_split
+
+load_dotenv()
 
 
 class RecommendService(ABC):
@@ -21,8 +27,12 @@ class RecommendService(ABC):
         pass
 
     @abstractmethod
-    def get_recommendations(self, user_id: int, n_recommendations: int = 5) -> list:
+    def get_user_recommendations(self, user_id: int, n_recommendations: int = 5) -> list:
         """Возвращает список рекомендованных фильмов для заданного пользователя."""
+        pass
+
+    def get_all_recommendations(self):
+        """Возвращает список рекомендованных фильмов для всех пользователей."""
         pass
 
 
@@ -30,6 +40,7 @@ class CollaborativeFilter(RecommendService):
 
     def train_model(self) -> list:
         """Обучает модель на основе данных рейтингов и возвращает предсказания."""
+
         reader = Reader(rating_scale=(1, 5))
         input_data = Dataset.load_from_df(self.ratings_df[['user_id', 'movie_id', 'rating']], reader)
 
@@ -42,7 +53,7 @@ class CollaborativeFilter(RecommendService):
 
         return predictions
 
-    def get_recommendations(self, user_id: str, n_recommendations: int = 5) -> list:
+    def get_user_recommendations(self, user_id: str, n_recommendations: int = 5) -> list:
         """Возвращает список рекомендованных фильмов для заданного пользователя."""
 
         all_movie_ids = self.ratings_df['movie_id'].unique()
@@ -55,16 +66,27 @@ class CollaborativeFilter(RecommendService):
         top_n_recommendations = predictions[:n_recommendations]
         return [(pred.iid, pred.est) for pred in top_n_recommendations]
 
+    def get_all_recommendations(self):
+        """Возвращает список рекомендованных фильмов для всех пользователей."""
+
+        all_users = self.ratings_df['user_id'].unique()
+        for user in all_users:
+            user_recommend = self.get_user_recommendations(user, n_recommendations=10)
+            logger.info(f'Рекомендация для пользователя {user}: {user_recommend}')
+            redis_db.set(user, json.dumps(user_recommend))
+            redis_db.expire(user, int(os.getenv('REDIS_EXPIRATION')))
+
+            # Пример получения списка рекомендаций
+            # get_list = json.loads(redis_db.get(user))
+            # logger.info(get_list)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
 
-    user_id = '27a06871-401f-4c17-b8d7-56540dc755cb'
+    redis_db = redis.StrictRedis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), db=0)
 
     recomend_data = CollaborativeFilter('matrix_surprise.csv')
     recomend_data.train_model()
-    recommendations = recomend_data.get_recommendations(user_id, n_recommendations=3)
-
-    logger.info(f'Рекомендации для пользователя: {recommendations}')
-
+    recomend_data.get_all_recommendations()
